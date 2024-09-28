5f213fac699ba2742871eb64841497d0f15d69ce
Safri Jawad <safrijawad1245@gmail.com>
2024-09-28 15:27:53 +0700

    build: moderation feature & make it sync

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,20 +5,31 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", async (req, res) => {
 	const event = req.body;
 
+	events.push(event);
+
 	axios.post("http://localhost:4000/events", event).catch((err) => {
-		console.log(err.message);
+		console.log("post error", err.message);
 	});
 	axios.post("http://localhost:4001/events", event).catch((err) => {
-		console.log(err.message);
+		console.log("comment error", err.message);
 	});
 	axios.post("http://localhost:4002/events", event).catch((err) => {
-		console.log(err.message);
+		console.log("query error", err.message);
+	});
+	axios.post("http://localhost:4003/events", event).catch((err) => {
+		console.log("moderation error", err.message);
 	});
 
 	res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+	res.send(events);
 });
 
 app.listen(4005, () => {
